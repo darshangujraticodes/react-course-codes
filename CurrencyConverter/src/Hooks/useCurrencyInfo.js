bab31a91136c88bc3f196d7608c79c929ece3179
darshangujraticodes <darshangujrati5@gmail.com>
2024-01-24 00:03:51 +0530

    removed unwanted files

--- CurrencyConverter/src/Hooks/useCurrencyInfo.js
+++ CurrencyConverter/src/Hooks/useCurrencyInfo.js
@@ -5,17 +5,21 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
   // fetch API Data
   const [data, setData] = useState({});
+  const [fetchingData, setFetchingData] = useState(false);
 
   // apiSignal help to identify signal type 200 or 404 help to identify error
   const controller = new AbortController();
   let apiSignal = controller.signal;
 
   useEffect(() => {
+    setFetchingData(true);
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`,
+      { apiSignal }
     )
       .then((res) => res.json())
       .then((apiInfo) => setData(apiInfo[currency]));
+
     // console.log(data, apiSignal);
   }, [currency]);
   return data;
